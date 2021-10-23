7b07bfb6ed934063d98e260f29da2642522eab19
Dapuya <78230285+Dapuya@users.noreply.github.com>
2021-10-23 12:13:00 +0600

    upd

--- lab 3/1.sql
+++ lab 3/1.sql
@@ -1,3 +1,23 @@
+-- --a
+-- select all course_id from course where credits>3;
+--
+-- --b
+-- select room_number from section where building = 'Watson' or building = 'Packard';
+--
+-- --c
+-- select course_id from course where dept_name = 'Comp. Sci.';
+--
+-- --d
+-- select course_id from section where semester = 'Fall';
+--
+-- --e
+-- select id, name from student where tot_cred > 45 and tot_cred < 90;
+--
+-- --f
+-- select name from student where substring(name, length(name),1) in ('a','e','u','o','i','y');
+--
+-- --g
+-- select course_id from prereq where prereq_id = 'CS-101';
 --a
 select all course_id from course where credits>3;
 
@@ -18,4 +38,3 @@ select name from student where substring(name, length(name),1) in ('a','e','u','
 
 --g
 select course_id from prereq where prereq_id = 'CS-101';
-
--- lab 3/3.sql
+++ lab 3/3.sql
@@ -1,3 +1,29 @@
+-- --a
+-- select s.id,name from student as s,takes as t,course as c
+-- where s.id=t.id
+--   and t.course_id=c.course_id
+--   and c.dept_name='Comp. Sci.'
+--   and t.grade in ('A','A-')
+-- order by name asc;
+--
+-- --b
+-- select distinct s_id from advisor as a,takes as t,student as s
+-- where t.id = a.s_id and s.id = t.id and t.grade in ('C-', 'C', 'C+', 'F');
+--
+-- --c
+-- select distinct dept_name from course
+-- except (select dept_name from course,takes
+-- where course.course_id=takes.course_id and takes.grade in ('F','C'));
+--
+-- --d
+-- select distinct id, name from instructor
+-- except (select instructor.id, instructor.name from instructor, takes, teaches
+-- where instructor.id = teaches.id and takes.course_id = teaches.course_id and  takes.grade = 'A');
+--
+-- --e
+-- select distinct time_slot.time_slot_id, title from section, course, time_slot
+-- where end_hr < 13 and section.course_id = course.course_id and section.time_slot_id = time_slot.time_slot_id
+-- order by title;
 --a
 select s.id,name from student as s,takes as t,course as c
 where s.id=t.id
