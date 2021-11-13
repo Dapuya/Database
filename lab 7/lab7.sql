e577f5b30573fd7ca4efa8eb57939ca251687c21
Dapuya <78230285+Dapuya@users.noreply.github.com>
2021-11-13 20:19:27 +0600

    lab 7

---
+++ lab 7/lab7.sql
@@ -0,0 +1,41 @@
+create table customers (
+    id integer primary key,
+    name varchar(255),
+    birth_date date
+);
+
+create table accounts(
+    account_id varchar(40) primary key ,
+    customer_id integer references customers(id),
+    currency varchar(3),
+    balance float,
+    "limit" float
+);
+
+create table transactions (
+    id serial primary key ,
+    date timestamp,
+    src_account varchar(40) references accounts(account_id),
+    dst_account varchar(40) references accounts(account_id),
+    amount float,
+    status varchar(20)
+);
+
+INSERT INTO customers VALUES (201, 'John', '2021-11-05');
+INSERT INTO customers VALUES (202, 'Anny', '2021-11-02');
+INSERT INTO customers VALUES (203, 'Rick', '2021-11-24');
+
+INSERT INTO accounts VALUES ('NT10204', 201, 'KZT', 1000, null);
+INSERT INTO accounts VALUES ('AB10203', 202, 'USD', 100, 0);
+INSERT INTO accounts VALUES ('DK12000', 203, 'EUR', 500, 200);
+INSERT INTO accounts VALUES ('NK90123', 201, 'USD', 400, 0);
+INSERT INTO accounts VALUES ('RS88012', 203, 'KZT', 5000, -100);
+
+INSERT INTO transactions VALUES (1, '2021-11-05 18:00:34.000000', 'NT10204', 'RS88012', 1000, 'commited');
+INSERT INTO transactions VALUES (2, '2021-11-05 18:01:19.000000', 'NK90123', 'AB10203', 500, 'rollback');
+INSERT INTO transactions VALUES (3, '2021-06-05 18:02:45.000000', 'RS88012', 'NT10204', 400, 'init');
+
+
+select * from customers;
+select * from accounts;
+select * from transactions;
---
+++ lab 7/tasks.sql
@@ -0,0 +1,88 @@
+-- 1
+-- SQL provides large object data types for character data (clob)
+-- and binary data (blob)
+
+-- 2
+-- Privileges control the ability to run SQL statements.
+-- A role is a privelege group, whereas a User is a person or Active Directory group.
+-- For example, an AD group "IT_Developers" may have Writer access to a database,
+-- but the AD group "Domain Administrators" aren't neccessarily admins on the database.
+
+-- a
+create role accountant;
+grant select on accounts, transactions to accountant;
+
+create role administrator;
+grant select, update on customers, accounts, transactions to administrator;
+
+create role support;
+grant all privileges on customers, accounts, transactions to support;
+
+-- b
+create user John;
+grant accountant to John;
+
+create user Oscar;
+grant accountant to Oscar;
+
+create user Anne;
+grant administrator to Anne;
+
+create user Pam;
+grant administrator to Pam;
+
+create user Michael;
+
+create user Jim;
+grant support to Jim;
+
+create user Dwight;
+
+-- c
+grant select, update on customers, accounts, transactions to Michael with grant option;
+grant select, update, insert on customers, accounts, transactions to Dwight with grant option;
+
+-- d
+revoke insert on accounts from Dwight;
+
+-- 3b
+alter table customers
+    alter column name set not null;
+
+alter table customers
+    alter column birth_date set not null;
+
+select * from customers;
+
+
+--5
+--a
+create unique index index_of_customers on accounts(customer_id, currency);
+--b
+create index index_of_transactions on accounts(currency, balance);
+
+
+-- 6
+do
+$$
+    declare bal int;
+    declare lim int;
+    begin
+--         savepoint s1;
+        update accounts
+        set balance = balance - 400
+        where account_id = 'RS88012';
+        update accounts
+        set balance = balance + 400
+        where account_id = 'NT10204';
+        select balance into bal from accounts where account_id = 'RS88012';
+        select accounts.limit into lim from accounts where account_id = 'RS88012';
+        if bal < lim then
+--             ROLLBACK TO SAVEPOINT s1;
+            update transactions set status = 'rollback' where id = 3;
+        else
+            commit;
+            update transactions set status = 'commited' where id = 3;
+        end if;
+    end;
+$$
